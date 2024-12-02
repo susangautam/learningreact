2da5a205f8aa49b8f0c59e79afced54feb8203ab
Susan Gautam <off.susangautam@gmail.com>
2024-12-02 18:18:35 +0545

    Header

--- src/Components/Navigation/Header.jsx
+++ src/Components/Navigation/Header.jsx
@@ -1,49 +1,43 @@
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
-import { FaTabletAlt } from "react-icons/fa";
+import { FaTabletAlt } from "react-icons/fa";   
 import { MdOutlinePhoneAndroid } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
-
+import { ImCross } from "react-icons/im";
 
 function Header() {
   return (
-   <div className="bg-#333333">
-    <div className="bg-tertiary text-white flex justify-between px-6 py-6">
-      <div>Email and Number</div>
-      <div><ul  className="flex flex-row gap-10 text-3xl">
-        <li><HiOutlineComputerDesktop /></li>
-        <li><FaTabletAlt /></li>
-        <li><MdOutlinePhoneAndroid /></li>simport { HiOutlineComputerDesktop } from "react-icons/hi2";
-import { FaTabletAlt } from "react-icons/fa";
-import { MdOutlinePhoneAndroid } from "react-icons/md";
-import { FaShoppingCart } from "react-icons/fa";
-
-function Header() {
-  return (
-    <div className="bg-[#333333]">
-      <div className="bg-tertiary text-white flex justify-between px-6 py-6">
-        <div className="flex-1">Email and Number</div>
-        <div className="flex justify-end">
-          <ul className="flex flex-row gap-10 text-3xl">
-            <li><HiOutlineComputerDesktop /></li>
-            <li><FaTabletAlt /></li>
-            <li><MdOutlinePhoneAndroid /></li>
-            <li className="bg-[#7AC64D]"><FaShoppingCart /></li>
+    <div style={{ backgroundColor: "#333333" }}>
+      <div className="bg-tertiary text-white flex justify-between ">
+        <div className=" flex gap-4" >
+          <div className="py-3 px-3"><img src="Logo.png" className="h-8"></img>
+          </div>
+          <div> <button className="py-4 px-6 text-center text-l bg-[#222222] border-gray-500  hover:text-white hover:border-pink-600 hover:border-r-2 hover:border-l-2 whitespace-pre">+  CLEVER</button>
+          </div>
+       
+        </div>
+        <div>
+          <ul className="flex flex-row text-2xl text-gray-400">
+            <li>
+              <button className="border-l-2 border-gray-500 px-4 py-4 hover:bg-[#222222] hover:text-white"><HiOutlineComputerDesktop /></button>
+            </li>
+            <li>
+              <button className="border-l-2 border-gray-500 px-4 py-4 hover:bg-[#222222] hover:text-white"><FaTabletAlt /></button>
+            </li>
+            <li>
+              <button className="border-l-2 border-gray-500 px-4 py-4 hover:bg-[#222222] hover:text-white"><MdOutlinePhoneAndroid /></button>
+            </li>
+            <li>
+              <button className="text-white px-4 py-4 bg-[#7AC64D] hover:bg-[#222222] hover:text-white"><FaShoppingCart /></button>
+            </li>
+            <li>
+              <button className=" border-gray-500 px-4 py-4  hover:bg-[#222222] hover:text-white"><ImCross /></button>
+            </li>
           </ul>
         </div>
       </div>
       <div className="bg-primary">Bottom</div>
     </div>
-  )
+  );
 }
 
-export default Header
-        <li className="bg-[#7AC64D]"><FaShoppingCart /></li>
-      </ul></div>
-    </div>
-    <div className="bg-primary">Bottom</div>
-    
-   </div>
-  )
-}
-
-export default Header
+export default Header;
